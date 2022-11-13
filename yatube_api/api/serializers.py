9acb650fbe6d4544aaa2146675ffb697cec3dc4a
Юлия Доманова <juliadomanova@yandex.ru>
2022-11-13 22:55:53 +0300

    Modified: api/serializers.py, yatube_api/urls.py, yatube_api/settings.py (adding base64 images)

--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -2,6 +2,8 @@ from rest_framework import serializers
 from rest_framework.relations import SlugRelatedField
 from rest_framework.validators import UniqueTogetherValidator
 from django.contrib.auth import get_user_model
+import base64
+from django.core.files.base import ContentFile
 
 from posts.models import Post, Group, Comment, Follow
 
@@ -9,8 +11,18 @@ from posts.models import Post, Group, Comment, Follow
 User = get_user_model()
 
 
+class Base64ImageField(serializers.ImageField):
+    def to_internal_value(self, data):
+        if isinstance(data, str) and data.startswith('data:image'):
+            format, imgstr = data.split(';base64,')  
+            ext = format.split('/')[-1]  
+            data = ContentFile(base64.b64decode(imgstr), name='temp.' + ext)
+        return super().to_internal_value(data)
+
+
 class PostSerializer(serializers.ModelSerializer):
     author = SlugRelatedField(slug_field='username', read_only=True)
+    image = Base64ImageField(required=False, allow_null=True)
 
     class Meta:
         fields = '__all__'
--- yatube_api/yatube_api/urls.py
+++ yatube_api/yatube_api/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import include, path
 from django.views.generic import TemplateView
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -11,3 +13,6 @@ urlpatterns = [
         name='redoc'
     ),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) 
